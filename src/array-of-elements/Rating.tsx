714c5cc2e138c209c27a3bce5d3c49a6e7c50ae0
Satyendra Singh <satyendra.singh@talentica.com>
2024-06-07 13:51:29 +0000

    rating component

--- src/array-of-elements/Rating.tsx
+++ src/array-of-elements/Rating.tsx
@@ -1,26 +1,38 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState } from 'react';
 
-const Rating = () => {
-  const [stars, setStars] = useState(-1);
+const Rating = ({ initRating = 0, maxRating = 10, onRatingChange = (i: number) => console.log(i) }) => {
+	const [rating, setRating] = useState(initRating);
 
-  const showRatings = () => {
-    const rating = [];
-    for (let i = 0; i < 5; i++) {
-      const styles = { color: 'white', fontSize: '28px' };
-      if (i <= stars) {
-        styles.color = 'yellow';
-      }
-      const el = (
-        <span style={styles} key={i} onClick={() => setStars(i)}>
-          O
-        </span>
-      );
-      rating.push(el);
-    }
-    return rating;
-  };
+	const defaultStyle = {
+		width: '40px',
+		height: '40px',
+		borderRadius: '20px',
+		backgroundColor: '#888888',
+	};
+	const updateRating = (i: number) => setRating(i);
+	const resetRating = () => setRating(initRating);
+	const showRatings = () => {
+		const ratings = [];
+		for (let i = 1; i <= maxRating; i++) {
+			ratings.push(
+				<button
+					key={i}
+					style={
+						i <= rating
+							? { ...defaultStyle, backgroundColor: 'orange' }
+							: defaultStyle
+					}
+					onMouseEnter={() => updateRating(i)}
+					onMouseLeave={() => resetRating()}
+					onClick={() => onRatingChange(i)}
+				></button>
+			);
+		}
+		return ratings;
+	};
 
-  return <div>{showRatings()}</div>;
+	return <div>{showRatings()}</div>;
 };
 
 export default Rating;
